39478b9e2f2e6be6cd74fbec74f11191ff30b4c3
SangKa.Z <sangka23@163.com>
2017-07-23 09:11:04 +0800

    chore(publish): add publish shell script

---
+++ publish.sh
@@ -0,0 +1,14 @@
+#!/usr/bin/env bash
+#
+# Generates production code and pushes it up to the site
+#
+
+rm -rf ./dist && \
+npm run build && \
+git checkout gh-pages && \
+git pull origin gh-pages && \
+cp -r ./dist ./ && \
+rm -rf ./dist && \
+git add . && \
+git commit -am "chore(publish): production code generated automatically" && \
+git push origin gh-pages
